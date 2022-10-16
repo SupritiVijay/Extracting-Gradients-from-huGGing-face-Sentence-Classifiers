0ce32f4b5f9a387be560dc9a37eeefc7bc8df3ba
AmanPriyanshu <aman.priyanshu@learner.manipal.edu>
2022-10-16 17:02:06 +0530

    main test

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from egggs_c import get_grad, word_importance_ranking
 import transformers
+import torch
 
 if __name__ == '__main__':
     model = transformers.AutoModelForSequenceClassification.from_pretrained("lvwerra/distilbert-imdb")
@@ -7,3 +8,10 @@ if __name__ == '__main__':
     output = get_grad(model, tokenizer, ["One of the other reviewers has mentioned that after watching just 1 Oz episode you'll be hooked."])
     importance_scores, reranked_words = word_importance_ranking(output, tokenizer)
     print(reranked_words)
+
+    output_0 = get_grad(model, tokenizer, ["One of the other reviewers has mentioned that after watching just 1 Oz episode you'll be hooked."], torch.tensor([0]))
+    importance_scores_0, reranked_words_0 = word_importance_ranking(output_0, tokenizer)
+    output_1 = get_grad(model, tokenizer, ["One of the other reviewers has mentioned that after watching just 1 Oz episode you'll be hooked."], torch.tensor([1]))
+    importance_scores_1, reranked_words_1 = word_importance_ranking(output_1, tokenizer)
+    print("0", reranked_words_0[:5])
+    print("1", reranked_words_1[:5])
